fb3bdbd071f28d4f6de0524fa43d79dd9a28b4ce
elyz081 <elyz0801@usherbrooke.ca>
2024-09-25 17:04:46 -0400

    adding project structure and script templates

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup, find_packages
+
+# Read the requirements from requirements.txt
+#with open('requirements.txt') as f:
+#    required = f.read().splitlines()
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open('requirements.txt') as f:
+    required_dependencies = f.read().splitlines()
+    external_dependencies = []
+    torch_added = False
+    for dependency in required_dependencies:
+        external_dependencies.append(dependency)
+
+setup(
+    name='TP1_IMN708',
+    version='0.1.0',
+    description='A short description of your project',
+    author='Chan Nam Nguyen, Zineb El yamani',
+    url='https://github.com/elyz081/TP1_IMN708.git',
+    packages=find_packages(),
+    install_requires=external_dependencies,
+    entry_points={
+        'console_scripts': [
+            'view = scripts.view_image:main',
+            'compute_mip_MIP = scripts.compute_mip_MIP:main',
+            'stats = scripts.show_image_stats:main',
+            'denoise = scripts.denoise_image:main',
+        ],
+    },
+)
