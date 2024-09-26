edfa83c26bafe238a604d4655a7a1a88128971d7
trivi-dimalab <triviuqtr@gmail.com>
2024-09-25 23:36:55 -0400

    Resolve merge conflit

--- scripts/view_image.py
+++ scripts/view_image.py
@@ -6,11 +6,7 @@ Description of what the script does
 
 import argparse
 import tools.utils
-<<<<<<< HEAD
 import tools.image
-=======
-
->>>>>>> 00a804f0f49e53ed5663e0b9e359b8151bb65107
 
 
 def _build_arg_parser():
@@ -19,13 +15,8 @@ def _build_arg_parser():
 
     p.add_argument('in_image',
                    help='Input image.')
-<<<<<<< HEAD
     p.add_argument('--axe', type=int, default=0,
                         help='Axe de la vue (Sagittale 0, Coronale 1, Axiale 2)')
-=======
-    p.add_argument('--optional', default=0,
-                   help='optional argument.')
->>>>>>> 00a804f0f49e53ed5663e0b9e359b8151bb65107
     
     tools.utils.add_verbose_arg(p)
 
@@ -35,7 +26,6 @@ def main():
     parser = _build_arg_parser()
     args = parser.parse_args()
 
-<<<<<<< HEAD
     # Read arguments
     in_image = args.in_image
     axe = args.axe
@@ -43,12 +33,6 @@ def main():
     # Call view function
     tools.image.afficher_image(in_image, axe)
         
-=======
-    #TODO replace the code here
-    print(args.in_image)
-    if args.optional:
-        print(args.optional)
->>>>>>> 00a804f0f49e53ed5663e0b9e359b8151bb65107
 
 if __name__ == "__main__":
     main()
