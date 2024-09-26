9e0f429e3bf657db8e828c3109f3ffe1df2e0129
chấn nam <47789251+cnam0203@users.noreply.github.com>
2024-09-25 23:39:04 -0400

    Update utils.py

--- tools/utils.py
+++ tools/utils.py
@@ -3,9 +3,4 @@ def add_verbose_arg(parser):
                         choices=['DEBUG', 'INFO', 'WARNING'], dest='verbose',
                         help='Produces verbose output depending on '
                              'the provided level. \nDefault level is warning, '
-<<<<<<< HEAD
                              'default when using -v is info.')
-=======
-                             'default when using -v is info.')
-
->>>>>>> 00a804f0f49e53ed5663e0b9e359b8151bb65107
