13f820765f6a6bd6d88728fdc6f0a9ceda252c88
elyz081 <elyz0801@usherbrooke.ca>
2024-09-30 16:04:48 -0400

    moving histogram computing to maths + stats

--- tools/math.py
+++ tools/math.py
@@ -62,3 +62,52 @@ def entropy(image):
     entropy = scipy.stats.entropy(counts)
 
     return entropy
+
+
+def display_histogram(data, bins, title='', min_range=None, max_range=None):
+    """
+    Display the histogram of the image data.
+    :param data: Numpy array of the image data.
+    :param bins: Number of bins for the histogram.
+    :param title: Title of the histogram plot.
+    """
+     # Apply the range filtering if min_range and max_range are provided
+    if min_range is not None:
+        data = data[data >= min_range]  # Keep data above or equal to min_range
+    if max_range is not None:
+        data = data[data <= max_range]
+    
+    plt.hist(data.flatten(), bins=bins, density=True, edgecolor='black')
+    plt.title(title)
+    plt.xlabel('Value')
+    plt.ylabel('Frequency')
+    plt.show()
+
+
+
+def histogram(data, bins, min_range, max_range, labels_data=None):
+
+    # Display the overall intensity histogram
+    display_histogram(data, bins, 'Intensity Histogram', min_range, max_range)
+
+    # If a mask is provided, plot histograms for each segment
+    if labels_data.any():        
+        # Extract unique region labels from the mask (excluding 0, which is background)
+        unique_labels = np.unique(labels_data[labels_data != 0]).astype(int)
+        std_bg = 0 
+        
+        # Iterate over each unique label (region of interest) in the mask
+        for label in unique_labels:
+            # Extract the data corresponding to the current RoI (label)
+            roi_data = data[labels_data == label]
+            display_histogram(roi_data, bins, f'Intensity Histogram for RoI {label}', min_range, max_range)
+            
+            if label == 1:
+                # For label 1 (assumed to be the background), calculate the standard deviation
+                std_bg = np.std(roi_data)
+                print(f'Standard deviation of intensity (background): {std_bg}')
+            else:
+                # For other RoIs, calculate the mean intensity and SNR
+                mean_roi = np.mean(roi_data)
+                snr = mean_roi / (std_bg + 1e-6)  # Avoid division by zero by adding a small value
+                print(f'RoI {label}, Mean of intensity: {mean_roi}, SNR: {snr}, min intensity: {np.min(roi_data)}, max intensity: {np.max(roi_data)}')
