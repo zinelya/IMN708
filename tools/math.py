1628d0377185e9303c58a270f0e455e6337caad8
Zineb El yamani <120342832+elyz081@users.noreply.github.com>
2024-10-03 21:37:55 -0400

    Update math.py

--- tools/math.py
+++ tools/math.py
@@ -3,30 +3,79 @@ import numpy as np
 import scipy
 
 
-def michelson(image):
-    "Calcule le contraste de Michelson d'une image"
-    Min = np.min(image[np.nonzero(image)])
-    Max = np.max(image)
+def michelson(data):
+    """
+    Computes the Michelson contrast of the image data.
+    
+    Parameters
+    ----------
+    data: numpy array
+        Numpy array of the image.
+    
+    Returns
+    -------
+    Michelson: float
+        Michelson contrast value of the image data.
+    """
+    #TODO
+    Min = np.min(data[np.nonzero(data)])
+    Max = np.max(data)
     M = (Max - Min)/(Max + Min)
     return M
 
-def rms(image):
-    "Calcule le contraste Root-Mean-Square d'une image"
-    RMS = np.std(image)
+def rms(data):    
+    """
+    Computes the RMS contrast of the image data.
+    
+    Parameters
+    ----------
+    data: numpy array
+        Numpy array of the image.
+    
+    Returns
+    -------
+    RMS: float
+        RMS contrast value of the image data.
+    """
+    RMS = np.std(data)
     return RMS
 
-def mIP(image, axe) :    
-    mIP = np.min(image, axis=axe)
+def mIP(data, axe) :
+    """
+    Computes the minimum Intensity Projection (mIP) along the given axis.
+    
+    Parameters
+    ----------
+    data: numpy array
+        Numpy array of the image.
+    axe: 0 or 1 or 2
+        Projection axis (0: Sagittal, 1: Coronal, 2: Axial).   
+
+    Returns
+    -------
+    mIP: numpy array
+        mIP of the image data.
+    """    
+    mIP = np.min(data, axis=axe)
     return mIP
  
 
 
-def MIP(image, axe) :    
-    MIP = np.max(image, axis=axe)
+def MIP(data, axe) :
+    """
+    Computes the Maximum Intensity Projection (MIP) along the given axis.
+    
+    Parameters
+    ----------
+    data : numpy array
+        Numpy array of the image.
+    axe: 0 or 1 or 2
+        Projection axis (0: Sagittal, 1: Coronal, 2: Axial).   
+
+    Returns
+    -------
+    MIP : numpy array
+        MIP of the image data.
+    """     
+    MIP = np.max(data, axis=axe)
     return MIP
-
-def entropy(image):
-    value,counts = np.unique(image, return_counts=True)
-    entropy = scipy.stats.entropy(counts)
-
-    return entropy
