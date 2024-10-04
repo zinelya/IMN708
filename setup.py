216f376ee8b37793e8497e7cb519f2c136a7ad9d
Zineb El yamani <120342832+elyz081@users.noreply.github.com>
2024-10-03 21:42:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 import os
-
 from setuptools import setup, find_packages
 
+
+# Path to the current directory
 here = os.path.abspath(os.path.dirname(__file__))
 
+# Read the required dependencies from the requirements.txt
 with open('requirements.txt') as f:
     required_dependencies = f.read().splitlines()
-    external_dependencies = []
-    torch_added = False
-    for dependency in required_dependencies:
-        external_dependencies.append(dependency)
 
+
+# Setup function
 setup(
     name='TP1_IMN708',
     version='0.1.0',
@@ -18,14 +18,13 @@ setup(
     author='Chan Nam Nguyen, Zineb El yamani',
     url='https://github.com/elyz081/TP1_IMN708.git',
     packages=find_packages(),
-    install_requires=external_dependencies,
+    install_requires=required_dependencies,
     entry_points={
         'console_scripts': [
             'view_image = scripts.view_image:main',
-            'compute_mip_MIP = scripts.compute_mip_MIP:main',
+            'mip_MIP = scripts.compute_mip_MIP:main',
             'stats = scripts.show_image_stats:main',
             'denoise = scripts.denoise_image:main',
-            'show_histogram = scripts.show_histogram:main',
         ],
     },
 )
