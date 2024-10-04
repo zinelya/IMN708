82343c6d44c0994f54b0a9fad60c2c17eae14264
Zineb El yamani <120342832+elyz081@users.noreply.github.com>
2024-10-03 21:39:46 -0400

    Update display.py

--- tools/display.py
+++ tools/display.py
@@ -2,50 +2,68 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.widgets import Slider
 
-def update_display(ax, data, current_slice, axe, voxel_sizes, is_4d, current_time):
+def update_display(axe, data, current_slice, axe_view, voxel_sizes, is_4d, current_time):
     """
     Update the displayed image on the given axis based on current slice and time point.
-    :param ax: Matplotlib axis to update.
-    :param data: Image data.
-    :param current_slice: Current slice to display.
-    :param axe: Axis for slice selection (0: Sagittal, 1: Coronal, 2: Axial).
-    :param voxel_sizes: Tuple of voxel sizes for aspect ratio adjustments.
-    :param is_4d: Boolean flag indicating if the data is 4D.
-    :param current_time: Current time point (for 4D data).
+
+    Parameters
+    ----------
+    axe: Matplotlib Axes object
+        Matplotlib axis to update.
+    data: numpy array.
+        Numpy array of the image to view.
+    current_slice: 
+        Current slice to display.
+    axe_view: 0 or 1 or 2
+        axis for slice selection (0: Sagittal, 1: Coronal, 2: axial).
+    voxel_sizes: tuple
+        Spatial resolution.
+    is_4d:  Bool
+        Boolean flag indicating if the data is 4D.
+    current_time: 
+        Current time point (for 4D data).
     """
-    ax.clear()  # Clear the previous image
+    axe.clear()  # Clear the previous image
 
     # Slice the data based on the selected axis and time point
-    if axe == 0:  # Sagittal
+    if axe_view == 0:  # Sagittal
         img_slice = data[current_slice, :, :, current_time] if is_4d else data[current_slice, :, :]
-    elif axe == 1:  # Coronal
+    elif axe_view == 1:  # Coronal
         img_slice = data[:, current_slice, :, current_time] if is_4d else data[:, current_slice, :]
-    elif axe == 2:  # Axial
+    elif axe_view == 2:  # Axial
         img_slice = data[:, :, current_slice, current_time] if is_4d else data[:, :, current_slice]
 
     # Transpose and flip if needed to maintain correct orientation
-    #transposed_slice = img_slice.T
-    #if axe > 0:
-        #transposed_slice = np.fliplr(transposed_slice)
-    transposed_slice = img_slice
-    # Set aspect ratio based on voxel sizes
-    aspect_ratios = [(voxel_sizes[1] / voxel_sizes[2]),  # Sagittal (X-axis)
-                     (voxel_sizes[0] / voxel_sizes[2]),  # Coronal (Y-axis)
-                     (voxel_sizes[0] / voxel_sizes[1])]  # Axial (Z-axis)
+    transposed_slice = img_slice.T
+    if axe_view > 0:
+        transposed_slice = np.fliplr(transposed_slice)
+
+    # Set aspect ratio based on voxel_sizesel sizes
+    aspect_ratios = [(voxel_sizes[1] / voxel_sizes[2]),  # Sagittal 
+                     (voxel_sizes[0] / voxel_sizes[2]),  # Coronal 
+                     (voxel_sizes[0] / voxel_sizes[1])]  # Axial 
 
     # Define projection titles
     titles = ['Sagittal', 'Coronal', 'Axial']
-    ax.imshow(transposed_slice, cmap='gray', origin='lower', aspect=aspect_ratios[axe])
-    ax.set_title(f'{titles[axe]} slice {current_slice}')
+    axe.imshow(transposed_slice, cmap='gray', origin='lower', aspect=aspect_ratios[axe_view])
+    axe.set_title(f'{titles[axe_view]} slice {current_slice}')
     plt.draw()
 
 
 def display_image(data, voxel_sizes, axe, titre='Image'):
     """
-    Display the image in 2D slices, allowing for scrolling through slices and time points if 4D.
-    :param in_image: NIfTI image file.
-    :param axe: Axis to display slices (0: Sagittal, 1: Coronal, 2: Axial).
-    :param titre: Title for the plot.
+    Display the image in 3D slices, allowing for scrolling through slices and time points if 4D.
+
+    Parameters
+    ----------
+    data: numpy array.
+        Numpy array of the image to view.
+    voxel_sizes: Tuple
+        Spatial resolution of the image in terms of voxel dimensions.
+    axe: (0: Sagittal, 1: Coronal, 2: Axial)
+        Axis to display slices.
+    titre: string
+        Title for the plot.
     """
     is_4d = data.ndim == 4
     current_time = 0
@@ -85,9 +103,40 @@ def display_image(data, voxel_sizes, axe, titre='Image'):
     plt.show()
 
 
-def display_stats(data, bins, title='', min_range=None, max_range=None, resolution=None, vox=None, unit=None, Michelson=None, RMS=None, std_bg=None, SNR=None):
+def display_stats(data, bins, title='', min_range=None, max_range=None, taille=None, voxel_size=None, unit=None, size_4d=None,  mean=None, Michelson=None, RMS=None, std_bg=None, std=None, SNR=None):
     """
-    Display the histogram and the stats of the image data.
+    Display the histogram and the statistics of the image data.
+
+    Parameters
+    ----------
+    data : numpy array
+        Numpy array of the image.
+    bins : int
+        Number of bins to be used for the histogram.
+    title : str
+        Title for the plot.
+    min_range : int
+        Minimum intensity value to be included in the histogram. 
+    max_range : int
+        Maximum intensity value to be included in the histogram.
+    taille : tuple of floats
+        Dimensions of the image in the format (width, height, depth). 
+    voxel_size : tuple of floats
+        Spatial resolution of the image in terms of voxel dimensions.
+    unit : str
+        Unit of measurement for the voxel size or image dimensions, e.g., "mm".
+    size_4d : int
+        Size of the 4th dimension
+    Michelson : float
+        Michelson contrast value of the image.
+    RMS : float
+        Root mean square (RMS) contrast of the image.
+    std_bg : float
+        Standard deviation of the background noise in the image.
+    std : float
+        Standard deviation.
+    SNR : float
+        Signal-to-noise ratio (SNR) of the image.
     """
     # Apply the range filtering if min_range and max_range are provided
     if min_range is not None:
@@ -103,21 +152,23 @@ def display_stats(data, bins, title='', min_range=None, max_range=None, resoluti
 
     # Collect the statistics in a string
     stats_text = ""
-    if resolution is not None:
-        stats_text += f"Resolution: {resolution[0]:.2f} x {resolution[1]:.2f} x {resolution[2]:.2f}\n"
-    if vox is not None:
-        stats_text += f"taille de l'image: {vox[0]} x {vox[1]} x {vox[2]}\n"
-    if unit is not None:
-        stats_text += f"Unité: {unit[0]}\n"
+    if taille is not None:
+        stats_text += f"Resolution:  {voxel_size[0]:.3f}{unit[0]}x {voxel_size[1]:.3f}{unit[0]}x {voxel_size[2]:.3f}{unit[0]}\n"
+    if voxel_size is not None:
+        stats_text += f"Taille de l'image: {taille[0]} x {taille[1]} x {taille[2]}\n" 
+    if size_4d is not None:
+        stats_text += f"Taille de la 4eme dimension: {size_4d}\n"
     if Michelson is not None:
-        stats_text += f"Michelson: {Michelson:.2f}\n"
+        stats_text += f"Michelson: {Michelson}\n"
     if RMS is not None:
-        stats_text += f"RMS: {RMS:.2f}\n"
+        stats_text += f"RMS: {RMS:.3f}\n"
     if std_bg is not None:
         stats_text += f"Ecart-type du background: {std_bg:.2f}\n"
+    if std is not None:
+        stats_text += f"Ecart-type de la ROI: {std:.2f}\n"
     if SNR is not None:
-        stats_text += f"SNR: {SNR:.2f}\n"
+        stats_text += f"SNR: {SNR:.3f}\n"
 
-    plt.gcf().text(0.75, 0.6, stats_text, fontsize=12, bbox=dict(facecolor='blue', alpha=0.5))
+    plt.gcf().text(0.72, 0.5, stats_text, fontsize=12, bbox=dict(facecolor='blue', alpha=0.5))
     plt.subplots_adjust(right=0.7)
     plt.show()
