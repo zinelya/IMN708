76e9449e1cf918450a44d3d4bbe960503944b306
elyz081 <elyz0801@usherbrooke.ca>
2024-10-05 13:45:38 -0400

    save_nifti_function

--- tools/io.py
+++ tools/io.py
@@ -55,3 +55,31 @@ def reorient_data_rsa(image):
         data = np.flip(data, axis=2)
 
     return data
+
+def save_nifti_image(data, affine, input_filename, method_name, parameters, output_dir):
+    """
+    Saves a numpy array as a nifti image with the following naming : 
+    t1.nii.gz denoised with nl_means [h=1, patch_size=5, patch_distance=8] --> t1.nl_means_1_5_8.nii.gz
+    t1.nii.gz denoised with anisotropic_diffusion [niter=10, kappa=50, gamma=0.1] --> t1.nl_means_10_50_0.1.nii.gz
+    
+    Parameters
+    ----------
+    data : numpy.ndarray
+        The denoised image data to be saved.
+    affine : numpy.ndarray
+        The affine transformation matrix of the intitial NIfTI image.
+    input_filename : str
+        The original input filename from which the base name is extracted.
+    method_name : str
+        The denoising method name to append to the output file.
+    parameters : List
+        List of parameters chosen for the method.
+    output_dir : str
+        The directory where the output file will be saved.
+    """
+    base_name = os.path.splitext(os.path.basename(input_filename))[0]  # Extract the base name from input file
+    output_path = os.path.join(output_dir, f"{base_name}_{method_name}.nii.gz")  # Append the method name
+    nifti_img = nib.Nifti1Image(data, affine)
+    nib.save(nifti_img, output_path)
+    print(f"Saved: {output_path}")
+    return None
