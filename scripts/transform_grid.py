5d2f2a73e9f8014b70ddc90c0fa6ef7831fd1c6c
trivi-dimalab <triviuqtr@gmail.com>
2024-11-03 15:04:13 -0500

    Initialize transform_grid.py

--- scripts/transform_grid.py
+++ scripts/transform_grid.py
@@ -52,8 +52,18 @@ def main():
     grid = tools.transform.initialize_grid_points(args.width, args.depth, args.height, args.x, args.y, args.z)
     transformed_grid = tools.transform.trans_rigide(grid, args.theta, args.omega, args.phi, args.p, args.q, args.r)
     scaled_grid = tools.transform.similitude(transformed_grid, args.s)
-
     tools.display.display_grids([grid, scaled_grid])
+    
+    # Test affine matrix multiplication
+    # affine_matrix_1 = np.array([[0.9045, -0.3847, -0.1840, 10], [0.2939,0.875, -0.3847, 10], [0.3090, 0.2939, 0.9045, 10], [0, 0, 0, 1]])
+    # affine_grid_1 = tools.transform.trans_with_affine_matrix(grid, affine_matrix_1)
+    
+    # affine_matrix_2 = np.array([[0, -0.2598, 0.15, -3], [0, -0.15, -0.2598, 1.5], [0.3, 0, 0, 0], [0, 0, 0, 1]])
+    # affine_grid_2 = tools.transform.trans_with_affine_matrix(grid, affine_matrix_2)
+    
+    # affine_matrix_3 = np.array([[0.7182, -1.3727, -0.5660, 1.8115], [-1.9236, -4.6556, -2.5512, 0.2873], [-0.6426, -1.7985, -1.6285, 0.7404], [0, 0, 0, 1]])
+    # affine_grid_3 = tools.transform.trans_with_affine_matrix(grid, affine_matrix_3)
+    # tools.display.display_grids([grid, affine_grid_1, affine_grid_2, affine_grid_3])
     return 
     
             
